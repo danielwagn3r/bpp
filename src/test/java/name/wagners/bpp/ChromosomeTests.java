67168d5d45fc0b8098534e920ae07fe89a475635
Daniel Wagner <daniel@wagners.name>
2012-07-15 17:59:40 +0000

    .

---
+++ src/test/java/name/wagners/bpp/ChromosomeTests.java
@@ -0,0 +1,126 @@
+/*
+ * Bpp - A Bin Packer in Java
+ *
+ * Copyright (C) 2008  Daniel Wagner <daniel@wagners.name>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: CrossoverRecombination.java 1236 2011-10-07 08:44:14Z Daniel $
+ */
+package name.wagners.bpp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Daniel Wagner <daniel@wagners.name>
+ *
+ */
+public class ChromosomeTests {
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Chromosome#Chromosome()}.
+	 */
+	@Test
+	public void testChromosome() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Chromosome#add(int)}.
+	 */
+	@Test
+	public void testAdd() {
+		Chromosome chrom = new Chromosome();
+
+		chrom.add(1);
+		assertEquals(1, chrom.size());
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Chromosome#clone()}.
+	 */
+	@Test
+	public void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Chromosome#get(int)}.
+	 */
+	@Test
+	public void testGet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Chromosome#remove(int)}.
+	 */
+	@Test
+	public void testRemove() {
+		Chromosome chrom = new Chromosome();
+
+		chrom.add(1);
+
+		assertEquals(1, chrom.size());
+
+		chrom.remove(1);
+
+		assertEquals(0, chrom.size());
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Chromosome#size()}.
+	 */
+	@Test
+	public void testSize() {
+		Chromosome chrom = new Chromosome();
+
+		assertEquals(0, chrom.size());
+
+		chrom.add(1);
+
+		assertEquals(1, chrom.size());
+
+		chrom.remove(1);
+
+		assertEquals(0, chrom.size());
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Chromosome#toArray()}.
+	 */
+	@Test
+	public void testToArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Chromosome#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Chromosome#weight()}.
+	 */
+	@Test
+	public void testWeight() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ src/test/java/name/wagners/bpp/DnaTests.java
@@ -0,0 +1,121 @@
+/*
+ * Bpp - A Bin Packer in Java
+ *
+ * Copyright (C) 2008  Daniel Wagner <daniel@wagners.name>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: CrossoverRecombination.java 1236 2011-10-07 08:44:14Z Daniel $
+ */
+package name.wagners.bpp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Daniel Wagner <daniel@wagners.name>
+ *
+ */
+public class DnaTests {
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Dna#recombineVariantA(name.wagners.bpp.Dna, name.wagners.bpp.Dna)}.
+	 */
+	@Test
+	public void testRecombineVariantA() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Dna#recombineVariantB(name.wagners.bpp.Dna, name.wagners.bpp.Dna)}.
+	 */
+	@Test
+	public void testRecombineVariantB() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Dna#Dna(boolean)}.
+	 */
+	@Test
+	public void testDnaBoolean() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Dna#Dna(int[])}.
+	 */
+	@Test
+	public void testDnaIntArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Dna#clone()}.
+	 */
+	@Test
+	public void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Dna#fitness()}.
+	 */
+	@Test
+	public void testFitness() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Dna#mutate()}.
+	 */
+	@Test
+	public void testMutate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Dna#repair()}.
+	 */
+	@Test
+	public void testRepair() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Dna#toArray()}.
+	 */
+	@Test
+	public void testToArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Dna#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link name.wagners.bpp.Dna#weight()}.
+	 */
+	@Test
+	public void testWeight() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
